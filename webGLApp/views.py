40d19f37b5a78c531b09913b32aea708e53aef46
summericeyl <summericeyl@gmail.com>
2018-01-29 21:09:47 +0800

    删除一些不必要的应用

--- webGLApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth import views as auth_views
-
-# Create your views here.
-
-def index(request):
-  return render(request, 'webGLApp/main.html', {})
-
